268be4c6b9c1cc3718f06883a0e59014c7459e57
Ben Everard <hello@beneverard.co.uk>
2012-10-27 01:05:27 +0100

    Modified front page structure

--- front-page.php
+++ front-page.php
@@ -2,57 +2,26 @@
 
 <?php get_header(); ?>
 	
-	<div class="row home-profile">
+	<?php if ( have_posts() ) : the_post(); ?>
 
-		<div class="span_3 col">
-			<img src="<?php bloginfo('template_directory'); ?>/assets/images/avatar.jpg" />
+		<div class="row home-profile">
+
+			<div class="span_3 col">
+				<img src="<?php bloginfo('template_directory'); ?>/assets/images/avatar.jpg" />
+			</div>
+
+			<div class="span_1 col"><img src="<?php bloginfo('template_directory'); ?>/assets/images/pencil-arrow.png" class="no-border arrow" /></div>
+			<div class="span_8 col">
+
+				<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+
+			</div>
+
 		</div>
 
-		<div class="span_1 col"><img src="<?php bloginfo('template_directory'); ?>/assets/images/pencil-arrow.png" class="no-border arrow" /></div>
-		<div class="span_8 col">
-
-			<h2>Hello, I'm Ben!</h2>
-			<p>Now we’ve sorted that out, let me interest you with a few more facts. I’m a 24 year web developer from Taunton, England. I’m pushing 6’ 5”, have a keen interest in motorsport photography and hold awards in being pedantic and fasitious.</p>
-			<p>I am <a href="#">available for hire</a>!</p>
-
-		</div>
-
-	</div>
+	<?php endif; ?>
 
 	<hr />
-
-	<div class="row">
-		<div class="span_4 col">Header</div>
-		<div class="span_4 col">Header</div>
-		<div class="span_4 col">Header</div>
-	</div>
-	<div class="row">
-		<div class="span_3 col">Header</div>
-		<div class="span_3 col">Header</div>
-		<div class="span_3 col">Header</div>
-		<div class="span_3 col">Header</div>
-	</div>
-	<div class="row">
-		<div class="span_2 col">Header</div>
-		<div class="span_2 col">Header</div>
-		<div class="span_2 col">Header</div>
-		<div class="span_2 col">Header</div>
-		<div class="span_2 col">Header</div>
-		<div class="span_2 col">Header</div>
-	</div>
-	<div class="row">
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-		<div class="span_1 col">Header</div>
-	</div>
-
+	
 <?php get_footer(); ?>
