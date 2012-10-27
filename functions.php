4b1b8074a57e4bcfff7cfd85375ad5e99dd8a4ea
Ben Everard <hello@beneverard.co.uk>
2012-10-27 01:04:19 +0100

    Tidied up functions, add excerpt filters

--- functions.php
+++ functions.php
@@ -1,9 +1,33 @@
 <?php // functions.php
 
 
- //*********************
+ //**************
 // THEME SUPPORT
 
-	add_theme_support('post-thumbnails');
-	add_theme_support('menus');
-	
+add_theme_support('post-thumbnails');
+add_theme_support('menus');
+
+
+ //****************
+// THUMBNAIL SIZES
+
+add_image_size('sidebar-featured', 220, 9999, FALSE);
+
+
+ //******
+// HACKS
+
+// disable admin bar
+add_filter('show_admin_bar', '__return_false'); 
+
+
+
+// excerpt filters
+
+add_filter('excerpt_length', function () { 
+	return 40;
+});
+
+add_filter('excerpt_more', function () {
+	return '...';
+});
