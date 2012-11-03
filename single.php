36ef66b125e5196fa0f2ce224b976fb21881ac29
Ben Everard <hello@beneverard.co.uk>
2012-11-03 14:51:56 +0000

    Added loop template to single template

--- single.php
+++ single.php
@@ -2,12 +2,6 @@
 
 <?php get_header(); ?>
 
-	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-		<h2><?php the_title(); ?></h2>
-
-		<?php the_content(); ?>
-
-	<?php endwhile; ?>
+	<?php get_template_part('loop'); ?>
 
 <?php get_footer(); ?>
