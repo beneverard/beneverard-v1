956f29f984bc40a3df584b8c4566c2a1bd4167b6
Ben Everard <hello@beneverard.co.uk>
2012-11-03 15:09:09 +0000

    Modified loop html structure

--- loop.php
+++ loop.php
@@ -4,28 +4,21 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<article class="row">
-
-			<div class="col span_3">
-				<h3><?php the_time('j F y'); ?></h3>
-			</div>
-
-			<div class="col span_9">
-
-				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-				
-				<?php the_excerpt(); ?>
-				<p><a href="<?php the_permalink(); ?>">Read the rest of this entry &raquo;</a></p>
-
-			</div>
-
-		</article>
+		<?php get_template_part('content', get_post_format()); ?>
 
 	<?php endwhile; ?>
 
-	<?php if (next_posts_link() || previous_posts_link()): ?>
-		<?php next_posts_link('&laquo; Older Entries') ?> | <?php previous_posts_link('Newer Entries &raquo;') ?>
-	<?php endif ?>
+	<div class="row">
+
+		<div class="col span_3">&nbsp;</div>
+
+		<div class="col span_9">
+
+			<?php if (next_posts_link() || previous_posts_link()): ?>
+				<?php next_posts_link('&laquo; Older Entries') ?> | <?php previous_posts_link('Newer Entries &raquo;') ?>
+			<?php endif ?>
+
+		</div>
 
 <?php else : ?>
 
