a9e709450efefd9d7a3861d5f9a4a12638761ddc
Ben Everard <hello@beneverard.co.uk>
2012-11-03 15:08:41 +0000

    Modified page template structure

--- page.php
+++ page.php
@@ -4,21 +4,31 @@
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
-		<div class="row / post">
+		<article>
 
-			<div class="col span_12">
-				<h2><?php the_title(); ?></h2>
+			<div class="row">
+
+				<div class="col span_3">&nbsp;</div>
+
+				<div class="col span_9">
+					<h2><?php the_title(); ?></h2>
+				</div>
+
 			</div>
 
-			<div class="col span_3 / sidebar">
-				<?php the_post_thumbnail('sidebar-featured'); ?>
+			<div class="row">
+
+				<div class="col span_3 / sidebar">
+					<?php the_post_thumbnail('sidebar-featured'); ?>
+				</div>
+
+				<div class="col span_9 / content">
+					<?php the_content(); ?>
+				</div>
+
 			</div>
 
-			<div class="col span_9 / content">
-				<?php the_content(); ?>
-			</div>
-
-		</div>
+		</article>
 
 	<?php endif; ?>
 
